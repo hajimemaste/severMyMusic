5630b33ba1f48bf2cb77b0f630f5f8b68dfde655
Hung <my@email.com>
2023-10-19 22:53:49 +0700

    sever

--- router/mySongRouter.js
+++ router/mySongRouter.js
@@ -3,7 +3,7 @@ const mySongControllers = require("../controllers/mySongController");
 const router = require("express").Router();
 
 router.get("/", mySongControllers.getListSong);
-router.post("/", mySongControllers.addSong);
+router.post("/create", mySongControllers.addSong);
 router.delete("/:id", mySongControllers.deleteSong);
 router.put("/:id", mySongControllers.updateSong);
 
